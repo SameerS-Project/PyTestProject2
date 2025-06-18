ac866c8a09933234635185d0cb8840518e8fded5
Sameer Siddiqui <ssiddiqui1993@gmail.com>
2025-06-18 17:41:32 -0500

    Updated HTML report and Ecom password

--- utilities/test_data.py
+++ utilities/test_data.py
@@ -2,4 +2,4 @@
 class TestData:
     url =       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login"
     email =     "postrona93@gmail.com"
-    password =  "S@m33r9300"
+    password =  "123@Best"
